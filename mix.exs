f070e988368dc392cb9cce6c3ed6a7afed7cc7bc
SamuelMoraisdRS <samuelmorais552@gmail.com>
2024-05-14 14:39:31 -0300

    Elixir version set to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MapReduce.MixProject do
     [
       app: :map_reduce,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
