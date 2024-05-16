34c14c15af2df2bd7d0020cc3f568c4221ae823e
rafael <rafael.19.rosendo@gmail.com>
2024-05-16 09:02:02 -0300

    esbouço

--- lib/map_reduce.ex
+++ lib/map_reduce.ex
@@ -3,4 +3,30 @@ defmodule MapReduce do
     System.schedulers()
     # System.schedulers_online()
   end
+
+  #DIVIDIR DATASET
+
+  defp recebe_map() do
+    #Map here 
+    # EX: doubled_list = Enum.map(list, fn x -> x * 2 end)
+
+    end
+
+  defp recebe_reduce() do
+    #Reduce here
+    #EX: sum = Enum.reduce(list, fn x, acc -> x + acc end)
+    end
+
+  defp recebe_threads_map() do
+
+    #recebe threads da map
+    end
+
+  defp shuffle(list) when is_list(list) do
+    #ordenar com base na chave
+
+    end
+
+
+
 end
