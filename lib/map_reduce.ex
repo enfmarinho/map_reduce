54ff1470e7545a1fb8acb19b745d3cf981c70122
enfmarinho <eduardonestormarinho228@gmail.com>
2024-05-23 15:28:51 -0300

    Reorganizing source code.

--- lib/map_reduce.ex
+++ lib/map_reduce.ex
@@ -1,30 +1,56 @@
 defmodule MapReduce do
+  # Chamada para o caso geral (com lista)
+  def main(list, map_func, reduce_func, acc) when is_list(list) do
+    list
+    |> split_lista()
+    |> master(map_func, reduce_func, acc)
+  # |> saida()
+  end
+  # Chamada recebendo o caminho de um arquivo (para o caso de fazer a contagem de palavras)
+  def main(file_path \\ "test.txt", map_func, reduce_func, acc) do
+    dividir_dataset(file_path)
+    |> main(map_func, reduce_func, acc)
+  end
+
+  defp master(list, fun_map, fun_reduce,acc) do
+    map_manager(list, fun_map)
+    receber_msgs(length(list))
+    |> concatena()
+
+    list = shuffle_sort(list, :id)
+    {first, second} = Enum.split(1, list)
+    particoes = dividir_dataset(second, first, [], [first])
+
+    # TODO terminar a parte do map
+    # reduce_manager(particoes, fun_reduce, _)
+  end
+
   def number_of_native_threads do
     System.schedulers()
     # System.schedulers_online()
   end
  
-  def receber_msgs(msgs \\ [], num_msgs) when num_msgs > 0 do
+  defp receber_msgs(msgs \\ [], num_msgs) when num_msgs > 0 do
     receive do
        msg -> receber_msgs([msg | msgs], num_msgs - 1)
     end
   end
-  def receber_msgs(msgs, num_msgs)  do
+  defp receber_msgs(msgs, num_msgs)  do
     msgs
   end
 
-  def concatena([], curr) do
+  defp concatena([], curr) do
      curr
   end 
-  def concatena([head | tail], curr \\ []) do
+  defp concatena([head | tail], curr \\ []) do
     concatena(tail, head ++ curr)
   end
 
   # Divide a lista de termos em uma lista de listas
-  def formar_listas(list, num) when num >= length(list) do
+  defp formar_listas(list, num) when num >= length(list) do
     [list]
   end
-  def formar_listas(list, num) do
+  defp formar_listas(list, num) do
     nova_list =
       list
       |> Enum.split(num)
@@ -40,7 +66,7 @@ defmodule MapReduce do
   end
 
   # Essa funcao é usada para o caso de contagem de palavras
-  def dividir_dataset(file_path)  do
+  defp dividir_dataset(file_path)  do
     file_path
     |> File.read()
     |> elem(1)
@@ -48,56 +74,35 @@ defmodule MapReduce do
     |> String.split(" ")
   end
 
-  defp master(list, fun_map, fun_reduce,acc) do
-    map_manager(list, fun_map)
-    receber_msgs(length(list))
-    |> concatena()
-
-    list = shuffle_sort(list, :id)
-    {first, second} = Enum.split(1, list)
-    particoes = dividir_dataset(second, first, [], [first])
-
-    # TODO terminar a parte do map
-    reduce_manager(particoes, fun_reduce, _)
-  end
-
-  def recebe_map([], fun) do
+  defp recebe_map([], fun) do
     []
   end
-  def recebe_map([h|t], fun) do
+  defp recebe_map([h|t], fun) do
     [fun.(h)] ++ recebe_map(t, fun)
   end
 
-  # 'acc' é o valor neutro da operacao (Deve ser alterado)
-  def recebe_reduce([], _) do
-    # acc
+  # 'pid_list' é uma lista contendo os PIDs das threads utlizadas (será necesssario para receber as mensagens)
+  defp map_manager([], _) do
+    :ok
   end
-  def recebe_reduce(list,fun, _) when length(list) == 1 do
-    hd(list)
-  end
-  # Essa é chamada normal utilizada a partir da segunda iteração, em que será utilizaodo o valor "acc"
-  def recebe_reduce(list, fun) when length(list) >= 2 do
-    # Separa os dois primeiros elementos da lista
-    list1 = list |> Enum.split(2) |> elem(0)
-    list2 = list |> Enum.split(2) |> elem(1)
-    fun.(Enum.at(list, 0), Enum.at(list, 1)) |> fun.(recebe_reduce(list2, fun))
+  defp map_manager([h|t], fun_map) do
+    # Adiciona o novo PID à lista  
+    spawn(MapReduce, :concurrent_map, [h, fun_map, self])
+    map_manager(t, fun_map)
   end
 
-  # Esaa é a chamada equivalnete a primeira chamada do reduce, em que teremos que calcular
-  # o primeiro temro da lista com o 'acc'
-  def recebe_reduce([h|t], fun, acc) do
-    # Separa os dois primeiros elementos da lista
-    fun.(acc, h) |> fun.(recebe_reduce(t, fun))
+  defp concurrent_map(list ,fun_map, pid) do
+    send pid, recebe_map(list, fun_map)
   end
 
-  def shuffle_sort(maps , keys) do
+  defp shuffle_sort(maps , keys) do
     maps 
     |> Enum.shuffle()
     |> Enum.sort_by(&Map.get(&1, keys))
   end
 
-  def dividir_dataset([], _, listaLista, lista), do: listaLista ++ lista
-  def dividir_dataset(data, anterior, listaLista, lista) do
+  defp dividir_dataset([], _, listaLista, lista), do: listaLista ++ lista
+  defp dividir_dataset(data, anterior, listaLista, lista) do
     {first, second} = Enum.split(data, 1)
     if anterior == first do
       dividir_dataset(second, first, listaLista, lista ++ first)
@@ -106,42 +111,35 @@ defmodule MapReduce do
     end
   end
 
-  # 'pid_list' é uma lista contendo os PIDs das threads utlizadas (será necesssario para receber as mensagens)
-  def map_manager([], _) do
-    :ok
+  # 'acc' é o valor neutro da operacao (Deve ser alterado)
+  defp recebe_reduce([], _) do
+    # acc
   end
-  def map_manager([h|t], fun_map) do
-    # Adiciona o novo PID à lista  
-    spawn(MapReduce, :concurrent_map, [h, fun_map, self])
-    map_manager(t, fun_map)
+  defp recebe_reduce(list,fun, _) when length(list) == 1 do
+    hd(list)
+  end
+  # Essa é chamada normal utilizada a partir da segunda iteração, em que será utilizaodo o valor "acc"
+  defp recebe_reduce(list, fun) when length(list) >= 2 do
+    # Separa os dois primeiros elementos da lista
+    list1 = list |> Enum.split(2) |> elem(0)
+    list2 = list |> Enum.split(2) |> elem(1)
+    fun.(Enum.at(list, 0), Enum.at(list, 1)) |> fun.(recebe_reduce(list2, fun))
+  end
+  # Esaa é a chamada equivalnete a primeira chamada do reduce, em que teremos que calcular
+  # o primeiro temro da lista com o 'acc'
+  defp recebe_reduce([h|t], fun, acc) do
+    # Separa os dois primeiros elementos da lista
+    fun.(acc, h) |> fun.(recebe_reduce(t, fun))
   end
 
-  def concurrent_map(list ,fun_map, pid) do
-    send pid, recebe_map(list, fun_map)
-  end
-
-  def reduce_manager([], _, _) do
+  defp reduce_manager([], _, _) do
     :ok
   end
-  def reduce_manager([h|t], fun_red, acc) do
+  defp reduce_manager([h|t], fun_red, acc) do
     spawn(MapReduce, :concurrent_reduce, [h, fun_red, acc, self])
     reduce_manager(t, fun_red, acc)
   end
-  def concurrent_reduce(list, fun_red, acc,pid) do
+  defp concurrent_reduce(list, fun_red, acc,pid) do
     send pid, recebe_reduce(list,fun_red, acc)
   end
-
-  # Chamada para o caso geral (com lista)
-  def main(list, map_func, reduce_func, acc) when is_list(list) do
-    list
-    |> split_lista()
-    |> master(map_func, reduce_func, acc)
-  # |> saida()
-  end
-  # Chamada recebendo o caminho de um arquivo (para o caso de fazer a contagem de palavras)
-  def main(file_path \\ "test.txt", map_func, reduce_func, acc) do
-    dividir_dataset(file_path)
-    |> main(map_func, reduce_func, acc)
-  end
-
 end
