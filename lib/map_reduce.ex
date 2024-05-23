2655048b953df7fe012772e2c63ca5756dca075d
enfmarinho <eduardonestormarinho228@gmail.com>
2024-05-23 16:00:32 -0300

    Saving changes.

--- lib/map_reduce.ex
+++ lib/map_reduce.ex
@@ -19,39 +19,29 @@ defmodule MapReduce do
 
     list = shuffle_sort(list, :id)
     {first, second} = Enum.split(1, list)
-    particoes = dividir_dataset(second, first, [], [first])
+    # TODO checar funcar dividir_dataset (nao consigo compilar por causa da versao do meu compilador)
+    particoes = dividir_dataset(second, hd(first), [], [first])
 
-    # TODO terminar a parte do map
+    # TODO terminar reduce
     reduce_manager(particoes, fun_reduce, acc)
     receber_msgs(length(list))
     # TODO retornar resultado
   end
 
-  def number_of_native_threads do
-    System.schedulers()
-    # System.schedulers_online()
-  end
- 
+  defp number_of_native_threads(), do: System.schedulers()
+
   defp receber_msgs(msgs \\ [], num_msgs) when num_msgs > 0 do
     receive do
        msg -> receber_msgs([msg | msgs], num_msgs - 1)
     end
   end
-  defp receber_msgs(msgs, num_msgs)  do
-    msgs
-  end
+  defp receber_msgs(msgs, num_msgs), do: msgs
 
-  defp concatena([], curr) do
-     curr
-  end 
-  defp concatena([head | tail], curr \\ []) do
-    concatena(tail, head ++ curr)
-  end
+  defp concatena([], curr), do: curr
+  defp concatena([head | tail], curr \\ []), do: concatena(tail, head ++ curr)
 
   # Divide a lista de termos em uma lista de listas
-  defp formar_listas(list, num) when num >= length(list) do
-    [list]
-  end
+  defp formar_listas(list, num) when num >= length(list), do: [list]
   defp formar_listas(list, num) do
     nova_list =
       list
@@ -77,9 +67,7 @@ defmodule MapReduce do
   end
 
   # 'pid_list' é uma lista contendo os PIDs das threads utlizadas (será necesssario para receber as mensagens)
-  defp map_manager([], _) do
-    :ok
-  end
+  defp map_manager([], _), do: :ok
   defp map_manager([h|t], fun_map) do
     # Adiciona o novo PID à lista  
     spawn(MapReduce, :concurrent_map, [h, fun_map, self])
@@ -90,9 +78,7 @@ defmodule MapReduce do
     send pid, recebe_map(list, fun_map)
   end
 
-  defp recebe_map([], fun) do
-    []
-  end
+  defp recebe_map([], fun), do: []
   defp recebe_map([h|t], fun) do
     [fun.(h)] ++ recebe_map(t, fun)
   end
@@ -113,23 +99,26 @@ defmodule MapReduce do
     end
   end
 
-  defp reduce_manager([], _, _) do
-    :ok
-  end
+  defp reduce_manager([], _, _), do: :ok
   defp reduce_manager([h|t], fun_red, acc) do
     spawn(MapReduce, :concurrent_reduce, [h, fun_red, acc, self])
     reduce_manager(t, fun_red, acc)
   end
+
   defp concurrent_reduce(list, fun_red, acc,pid) do
     send pid, recebe_reduce(list, fun_red, acc)
   end
 
+  defp recebe_reduce(list,fun, _) when length(list) == 1, do: hd(list)
+  # Esaa é a chamada equivalnete a primeira chamada do reduce, em que teremos que calcular
+  # o primeiro temro da lista com o 'acc'
+  defp recebe_reduce([h|t], fun, acc) do
+    # Separa os dois primeiros elementos da lista
+    fun.(acc, h) |> fun.(recebe_reduce(t, fun))
+  end
   # 'acc' é o valor neutro da operacao (Deve ser alterado)
   defp recebe_reduce([], _) do
     # acc
-  end
-  defp recebe_reduce(list,fun, _) when length(list) == 1 do
-    hd(list)
   end
   # Essa é chamada normal utilizada a partir da segunda iteração, em que será utilizaodo o valor "acc"
   defp recebe_reduce(list, fun) when length(list) >= 2 do
@@ -137,11 +126,5 @@ defmodule MapReduce do
     list1 = list |> Enum.split(2) |> elem(0)
     list2 = list |> Enum.split(2) |> elem(1)
     fun.(Enum.at(list, 0), Enum.at(list, 1)) |> fun.(recebe_reduce(list2, fun))
-  end
-  # Esaa é a chamada equivalnete a primeira chamada do reduce, em que teremos que calcular
-  # o primeiro temro da lista com o 'acc'
-  defp recebe_reduce([h|t], fun, acc) do
-    # Separa os dois primeiros elementos da lista
-    fun.(acc, h) |> fun.(recebe_reduce(t, fun))
   end
 end
