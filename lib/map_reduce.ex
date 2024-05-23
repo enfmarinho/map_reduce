f114e0017c24d54552c140dbfb10cdc93147d33c
SamuelMorais <samuelmorais552@gmail.com>
2024-05-23 12:58:45 -0300

    Reduce concorrente e recebimento de mensagens

--- lib/map_reduce.ex
+++ lib/map_reduce.ex
@@ -3,9 +3,21 @@ defmodule MapReduce do
     System.schedulers()
     # System.schedulers_online()
   end
+ 
+  # Deve ter um jeito melhor de receber
+  def receber_msgs(msgs \\ [], num_msgs) when num_msgs > 0 do
+    receive do
+       msg -> receber_msgs([msg | msgs], num_msgs - 1)
+    end
+  end
+  def receber_msgs(msgs, num_msgs)  do
+    msgs
+  end
 
-  def concatena([], curr),do: curr 
-  def concatena([head | tail], curr) do
+  def concatena([], curr) do
+     curr
+  end 
+  def concatena([head | tail], curr \\ []) do
     concatena(tail, head ++ curr)
   end
 
@@ -39,9 +51,16 @@ defmodule MapReduce do
     |> String.split(" ")
   end
 
-  defp master(list, fun_map, fun_reduce) do
-
-
+  defp master(list, fun_map, fun_reduce,acc) do
+  map_manager(list, fun_map)
+  receber_msgs(length(list))
+  |> concatena()
+    
+  # O que falta {
+    # |> shuffle
+    # |> split
+    # reduce_manager(list, fun_reduce, acc)
+  # }
     # JOGAR NOS MAPS
 
     # ENviar para os escravos
@@ -66,51 +85,76 @@ defmodule MapReduce do
     [fun.(h)] ++ recebe_map(t, fun)
   end
 
-  # 'acc' é o valor neutro da operacao
-  def recebe_reduce([], _, acc) do
-    acc
+  # 'acc' é o valor neutro da operacao (Deve ser alterado)
+  def recebe_reduce([], _) do
+    # acc
   end
   def recebe_reduce(list,fun, _) when length(list) == 1 do
     hd(list)
   end
-  def recebe_reduce(list, fun, acc) when length(list) >= 2 do
+  # Essa é chamada normal utilizada a partir da segunda iteração, em que será utilizaodo o valor "acc"
+  def recebe_reduce(list, fun) when length(list) >= 2 do
     # Separa os dois primeiros elementos da lista
     list1 = list |> Enum.split(2) |> elem(0)
     list2 = list |> Enum.split(2) |> elem(1)
-    fun.(Enum.at(list, 0), Enum.at(list, 1)) |> fun.(recebe_reduce(list2, fun, acc))
+    fun.(Enum.at(list, 0), Enum.at(list, 1)) |> fun.(recebe_reduce(list2, fun))
   end
+
+  # Esaa é a chamada equivalnete a primeira chamada do reduce, em que teremos que calcular
+  # o primeiro temro da lista com o 'acc'
+  def recebe_reduce([h|t], fun, acc) do
+    # Separa os dois primeiros elementos da lista
+    fun.(acc, h) |> fun.(recebe_reduce(t, fun))
+  end
+  
 
   defp recebe_threads_map() do
     # recebe threads da map
   end
 
-  def map_gerence(_, []) do
+  # 'pid_list' é uma lista contendo os PIDs das threads utlizadas (será necesssario para receber as mensagens)
+  def map_manager([], _) do
     :ok
   end
-  def map_gerence(fun_map, [h|t]) do
-      spawn(MapReduce, :concurrent_map, [h, fun_map, self])
-      map_gerence(fun_map, t)
+  def map_manager([h|t], fun_map) do
+    # Adiciona o novo PID à lista  
+    spawn(MapReduce, :concurrent_map, [h, fun_map, self])
+    map_manager(t, fun_map)
   end
 
   def concurrent_map(list ,fun_map, pid) do
     send pid, recebe_map(list, fun_map)
   end
 
-  defp shuffle(list) when is_list(list) do
-
+  def reduce_manager([], _, _) do
+    :ok
   end
-  # == Essa seria a função pública principal
+  def reduce_manager([h|t], fun_red, acc) do
+    spawn(MapReduce, :concurrent_reduce, [h, fun_red, acc, self])
+    reduce_manager(t, fun_red, acc)
+  end
+  def concurrent_reduce(list, fun_red, acc,pid) do
+    send pid, recebe_reduce(list,fun_red, acc)
+  end
+
+  # Ordena em funcao da chave, ja divindo em uma lista de listas
+  defp shuffle([h | t])  do
+    # if (elem(h,0) > elem(hd(t), 0)) do
+      
+    # end
+  end
+  # * Essa seria a função pública principal
   # Chamada para o caso geral (com lista)
-  def main(list, map_func, reduce_func) when is_list(list) do
+  def main(list, map_func, reduce_func, acc) when is_list(list) do
     list
     |> split_lista()
-    |> master(map_func, reduce_func)
+    |> master(map_func, reduce_func, acc)
   # |> saida()
   end
   # Chamada recebendo o caminho de um arquivo (para o caso de fazer a contagem de palavras)
-  def main(file_path \\ "test.txt", map_func, reduce_func) do
+  def main(file_path \\ "test.txt", map_func, reduce_func, acc) do
     dividir_dataset(file_path)
-    |> split_lista
+    |> main(map_func, reduce_func, acc)
   end
 
 end
